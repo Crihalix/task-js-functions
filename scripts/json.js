4901708c667cb3d51c9bf4413f58866ee73873d9
Crihalix <Crihalix@gmail.com>
2014-07-30 17:25:40 +0300

    task-js-functions

--- scripts/json.js
+++ scripts/json.js
@@ -27,15 +27,16 @@ var gallery = {
 		]
 	};
 
+
 		//	2. Написати функцію, що буде виводити на екран інформацію про передані картинки галереї. Та використовувати її для виведення результату роботи всіх функцій галереї.
+
 		function showGallery() {
 			for (var i = 0; i < gallery.images.length; i++) {
-				if (i === 4) {
-					console.log('This is new image gallery');
-				}
 				console.log(gallery.images[i]);
-			};
-		};
+			}
+				console.log('----------------------------'); //просто для отделения в консоле (что бы не слипалось)
+		}
+		showGallery();
 
 		// 1.1 додати слайд
 		function addNewImage (name, path, description, date) {
@@ -45,27 +46,80 @@ var gallery = {
 			newImg.description = description || "";
 			newImg.date = date || "";
 			gallery.images.push(newImg);
-//			showGallery();
 		}
 
-		addNewImage('cato-dog','images/cato_dog.png',"Fanny animall:)","2014-07-27T09:07:47.683Z");
-
-
-
-
+		//создание нового слайда
+		console.log('Создание новго слайда "cato-dog"');
+		addNewImage('cato-dog','images/cato_dog.png',"Fanny animal:)","2014-07-27T09:07:47.683Z");
 
 		showGallery();
 
 
-		// 1.2 редагувати слайд
-		console.log(gallery.images[1].name)
-
-		function editImage (oldName, newName) {
+		// 1.2 редагувати слайд (старый параметр меняем на новый)
+		function editImage(oldName, newParam) {
 			for (var i = 0; i < gallery.images.length; i++) {
 				if (gallery.images[i].name === oldName) {
-					gallery.images[i].name = newName;
+					gallery.images[i].name = newParam;
+				} else if (gallery.images[i].path === oldName) {
+					gallery.images[i].path = newParam;
+				} else if (gallery.images[i].description === oldName) {
+					gallery.images[i].description = newParam;
+				} else if(gallery.images[i].date === oldName) {
+					gallery.images[i].date = newParam || new Date() ;
 				}
-			};
+			}
 			showGallery();
 		}
+		console.log('меняем параметр "description" в слайда "cato-dog"');
+		editImage('Fanny animal:)','sad animal:(')
+
+
+		// 1.3 удалить слайд (по названии картинки)
+		function deleteImage (nameSlide) {
+
+			console.log('Всех слайдов ' + gallery.images.length);
+
+			for (var i = 0; i < gallery.images.length; i++) {
+				if (gallery.images[i].name === nameSlide) {
+					console.log('Вот этот слайд Вы удаляете "' + gallery.images[i].name + '"')
+					gallery.images.splice(i,1); 
+
+					console.log('Теперь всех слайдов ' + gallery.images.length);
+				}
+			}
+			showGallery();
+		}
+
+		console.log('Удаляем слайд "dinosaur"');
+		deleteImage('dinosaur');
+
+
+		// 3. Відсортувати картинки по зазначеному полю (поле передавати аргументом у функцію)
+	
+		function sortImages(methodSort) {
+			gallery.images.sort(function compareName(nameA, nameB){
+				return nameA[methodSort] > nameB[methodSort];
+			});	
+			
+		}
+			sortImages('description');
+			console.log('3. Отсортировано по имени "description"');
+			showGallery();
+			
+			sortImages('name');
+			console.log('3. Отсортировано по имени "name" и т.д.');
+			showGallery();
+
+		
+		// 4. Відфільтрувати картинки по переданому полю.
+		console.log('4. Відфільтрувати картинки по переданому полю')
+		function filterGallery(galleryArr, i) {
+			var filter = "";
+		       filter = JSON.stringify(galleryArr, [i]);
+			filter = JSON.parse(filter);
+		}
+		filterGallery(gallery.images, "description");
+		showGallery();
+ 
+
 // to be continue...
